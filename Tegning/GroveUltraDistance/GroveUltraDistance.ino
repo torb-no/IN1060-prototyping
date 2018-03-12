a8fe428138759ba45cb588362671b90e58d92861
Torbjørn Vik Lunde <torbjornlunde@users.noreply.github.com>
2018-03-12 19:24:20 +0100

    Shorter distance, faster

--- Tegning/GroveUltraDistance/GroveUltraDistance.ino
+++ Tegning/GroveUltraDistance/GroveUltraDistance.ino
@@ -5,13 +5,13 @@
 // MENS ARDUINO-EN ER PAA!
 // DE KAN BLI ODELAGT!
 
-#define ULTRA_LIMIT_LEN 50
+#define ULTRA_LIMIT_LEN 35
 
 Ultrasonic ultraTop(3); // D3
 Ultrasonic ultraBot(7); // D7
 Ultrasonic ultraLeft(8); // D8
 Ultrasonic ultraRight(6); // D6
-
+            
 int x, y;
 
 void setup() {
@@ -30,6 +30,6 @@ int ultraToPenVal(long backwardsVal, long forwardsVal) {
   int d1 = ULTRA_LIMIT_LEN - min(backwardsVal, ULTRA_LIMIT_LEN);
   int d2 = ULTRA_LIMIT_LEN - min(forwardsVal, ULTRA_LIMIT_LEN);
 
-  return (d2 - d1) * 10;
+  return (d2 - d1) * 15;
 }
 
